5796af6a254c9dab1ff8059f8ade5ceb96af136e
Eduardo Lavrinha <eduardolavrinha@gmail.com>
2022-05-04 23:17:38 -0300

    mapeamento de identidades com jpa

--- src/main/java/com/dio/santander/banklineapi/model/Correntista.java
+++ src/main/java/com/dio/santander/banklineapi/model/Correntista.java
@@ -1,8 +1,21 @@
 package com.dio.santander.banklineapi.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_correntista")
 public class Correntista {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@Column(length = 20)
 	private String nome;
+	@Column(length = 60)
 	private String cpf;
 	
 	private Conta conta;
