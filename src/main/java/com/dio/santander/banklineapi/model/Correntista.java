4278e3c674d15dd4bb15c4d0809369ecf913a114
Eduardo Lavrinha <eduardolavrinha@gmail.com>
2022-05-04 22:34:50 -0300

    criação das classes de modelos

---
+++ src/main/java/com/dio/santander/banklineapi/model/Correntista.java
@@ -0,0 +1,34 @@
+package com.dio.santander.banklineapi.model;
+
+public class Correntista {
+	private Integer id;
+	private String nome;
+	private String cpf;
+	
+	private Conta conta;
+	public Conta getConta() {
+		return conta;
+	}
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+}
